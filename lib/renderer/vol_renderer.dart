7184adc496c55677ac90e778a8bafeffc1a9bb9d
airon@tark.pro <airon@tark.pro>
2020-04-09 12:00:41 +0700

    vol_renderer: improve UI

--- lib/renderer/vol_renderer.dart
+++ lib/renderer/vol_renderer.dart
@@ -1,25 +1,54 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:k_chart/formatter.dart';
+
 import '../entity/volume_entity.dart';
 import '../renderer/base_chart_renderer.dart';
-import '../utils/number_util.dart';
+import 'base_chart_renderer.dart';
 
 class VolRenderer extends BaseChartRenderer<VolumeEntity> {
   double mVolWidth = ChartStyle.volWidth;
 
-  VolRenderer(Rect mainRect, double maxValue, double minValue,
-      double topPadding, int fixedLength)
-      : super(
-            chartRect: mainRect,
-            maxValue: maxValue,
-            minValue: minValue,
-            topPadding: topPadding,
-            fixedLength: fixedLength);
+  /// Used to draw a shortened int in the right text
+  /// like 100K instead of 100,000
+  final Formatter shortFormatter;
+  final String wordVolume;
+
+  //
+  Paint _volumeBadgeBackgroundPaint = Paint()
+    ..isAntiAlias = true
+    ..filterQuality = FilterQuality.high
+    ..style = PaintingStyle.fill
+    ..color = Colors.white10;
+
+  VolRenderer(
+    Rect mainRect,
+    double maxValue,
+    double minValue,
+    double topPadding,
+    int fixedLength, {
+    @required this.shortFormatter,
+    @required this.wordVolume,
+    String fontFamily,
+  }) : super(
+          chartRect: mainRect,
+          maxValue: maxValue,
+          minValue: minValue,
+          topPadding: topPadding,
+          fixedLength: fixedLength,
+          fontFamily: fontFamily,
+        );
 
   @override
-  void drawChart(VolumeEntity lastPoint, VolumeEntity curPoint, double lastX,
-      double curX, Size size, Canvas canvas) {
+  void drawChart(
+    VolumeEntity lastPoint,
+    VolumeEntity curPoint,
+    double lastX,
+    double curX,
+    Size size,
+    Canvas canvas,
+  ) {
     double r = mVolWidth / 2;
     double top = getVolY(curPoint.vol);
     double bottom = chartRect.bottom;
@@ -48,7 +77,7 @@ class VolRenderer extends BaseChartRenderer<VolumeEntity> {
 
   @override
   void drawText(Canvas canvas, VolumeEntity data, double x) {
-    TextSpan span = TextSpan(
+    /*TextSpan span = TextSpan(
       children: [
         TextSpan(
             text: "VOL:${NumberUtil.format(data.vol)}    ",
@@ -66,16 +95,66 @@ class VolRenderer extends BaseChartRenderer<VolumeEntity> {
     TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
     tp.layout();
     tp.paint(canvas, Offset(x, chartRect.top - topPadding));
+    */
+
+    TextSpan span = TextSpan(
+      text: wordVolume,
+      style: TextStyle(
+        fontSize: 10.0,
+        color: Colors.white.withOpacity(0.5),
+        fontWeight: FontWeight.bold,
+      ),
+    );
+    TextPainter tp = TextPainter(
+      text: span,
+      textDirection: TextDirection.ltr,
+    );
+    tp.layout();
+
+    final yLinePlusPadding =
+        chartRect.top - topPadding + rightTextAxisLinePadding;
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromLTRB(
+          x - 2,
+          yLinePlusPadding,
+          x - 2 + tp.width + 12,
+          yLinePlusPadding + tp.height + 8,
+        ),
+        Radius.circular(3.0),
+      ),
+      _volumeBadgeBackgroundPaint,
+    );
+
+    tp.paint(
+      canvas,
+      Offset(
+        x - 2 + 6,
+        yLinePlusPadding + 4,
+      ),
+    );
   }
 
   @override
   void drawRightText(canvas, textStyle, int gridRows) {
-    TextSpan span =
-        TextSpan(text: "${NumberUtil.format(maxValue)}", style: textStyle);
-    TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+    TextSpan span = TextSpan(
+      text: shortFormatter != null
+          ? shortFormatter(maxValue.toInt())
+          : ChartFormats.numberShort.format(maxValue),
+      style: textStyle,
+    );
+    TextPainter tp = TextPainter(
+      text: span,
+      textDirection: TextDirection.ltr,
+    );
     tp.layout();
     tp.paint(
-        canvas, Offset(chartRect.width - tp.width, chartRect.top - topPadding));
+      canvas,
+      Offset(
+        chartRect.width - tp.width - rightTextScreenSidePadding,
+        chartRect.top - topPadding + rightTextAxisLinePadding,
+      ),
+    );
   }
 
   @override
