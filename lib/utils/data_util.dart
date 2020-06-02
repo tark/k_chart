cf3dbf079bc7664f9df9aa6f4fd2d7d8cabfbf3c
airon@tark.pro <airon@tark.pro>
2020-06-02 18:36:19 +0700

    data_util.dart: add parameters to index calculations

--- lib/utils/data_util.dart
+++ lib/utils/data_util.dart
@@ -7,62 +7,76 @@ import '../entity/k_line_entity.dart';
 // ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import,camel_case_types
 class DataUtil {
   static calculate(
-    List<KLineEntity> dataList, [
+    List<KLineEntity> data, [
     List<int> maDayList = const [5, 10, 20],
-    int n = 20,
-    k = 2,
+    int bollCalcPeriod = 20,
+    int bollBandwidth = 2,
+    int macdShortPeriod = 12,
+    int macdLongPeriod = 26,
+    int macdMaPeriod = 9,
+    int kdjCalcPeriod = 9,
+    int kdjMaPeriod1 = 3,
+    int kdjMaPeriod2 = 3,
+    int rsiPeriod = 6, // 12 / 24
+    int wrPeriod = 14, // 20
   ]) {
-    _calcMA(dataList, maDayList);
-    _calcBOLL(dataList, n, k);
-    _calcVolumeMA(dataList);
-    _calcKDJ(dataList);
-    _calcMACD(dataList);
-    _calcRSI(dataList);
-    _calcWR(dataList);
+    _calcMA(data, maDayList);
+    _calcBOLL(data, bollCalcPeriod, bollBandwidth);
+    _calcVolumeMA(data);
+    _calcKDJ(data, kdjCalcPeriod, kdjMaPeriod1, kdjMaPeriod2);
+    _calcMACD(data, macdShortPeriod, macdLongPeriod, macdMaPeriod);
+    _calcRSI(data, rsiPeriod);
+    _calcWR(data, wrPeriod);
   }
 
   static _calcMA(List<KLineEntity> dataList, List<int> maDayList) {
+    if (dataList == null || dataList.isEmpty) {
+      return;
+    }
+
     List<double> ma = List<double>.filled(maDayList.length, 0);
 
-    if (dataList != null && dataList.isNotEmpty) {
-      for (int i = 0; i < dataList.length; i++) {
-        KLineEntity entity = dataList[i];
-        final closePrice = entity.close;
-        entity.maValueList = List<double>(maDayList.length);
+    for (int i = 0; i < dataList.length; i++) {
+      KLineEntity entity = dataList[i];
+      final closePrice = entity.close;
+      entity.maValueList = List<double>(maDayList.length);
 
-        for (int j = 0; j < maDayList.length; j++) {
-          ma[j] += closePrice;
-          if (i == maDayList[j] - 1) {
-            entity.maValueList[j] = ma[j] / maDayList[j];
-          } else if (i >= maDayList[j]) {
-            ma[j] -= dataList[i - maDayList[j]].close;
-            entity.maValueList[j] = ma[j] / maDayList[j];
-          } else {
-            entity.maValueList[j] = 0;
-          }
+      for (int j = 0; j < maDayList.length; j++) {
+        ma[j] += closePrice;
+        if (i == maDayList[j] - 1) {
+          entity.maValueList[j] = ma[j] / maDayList[j];
+        } else if (i >= maDayList[j]) {
+          ma[j] -= dataList[i - maDayList[j]].close;
+          entity.maValueList[j] = ma[j] / maDayList[j];
+        } else {
+          entity.maValueList[j] = 0;
         }
       }
     }
   }
 
-  static void _calcBOLL(List<KLineEntity> dataList, int n, int k) {
-    print('_calcBOLL - $n - $k - ${dataList?.length}');
-    _calcBOLLMA(n, dataList);
+  static void _calcBOLL(
+    List<KLineEntity> dataList,
+    int calcPeriod,
+    int bandwidth,
+  ) {
+    print('_calcBOLL - $calcPeriod - $bandwidth - ${dataList?.length}');
+    _calcBOLLMA(calcPeriod, dataList);
     for (int i = 0; i < dataList.length; i++) {
-      KLineEntity entity = dataList[i];
-      if (i >= n) {
+      final e = dataList[i];
+      if (i >= calcPeriod) {
         double md = 0;
-        for (int j = i - n + 1; j <= i; j++) {
+        for (int j = i - calcPeriod + 1; j <= i; j++) {
           double c = dataList[j].close;
-          double m = entity.bollMa;
+          double m = e.bollMa;
           double value = c - m;
           md += value * value;
         }
-        md = md / (n - 1);
+        md = md / (calcPeriod - 1);
         md = sqrt(md);
-        entity.mb = entity.bollMa;
-        entity.up = entity.mb + k * md;
-        entity.dn = entity.mb - k * md;
+        e.bollMiddle = e.bollMa;
+        e.bollUp = e.bollMiddle + bandwidth * md;
+        e.bollDown = e.bollMiddle - bandwidth * md;
       }
     }
   }
@@ -88,9 +102,14 @@ class DataUtil {
     }
   }
 
-  static void _calcMACD(List<KLineEntity> dataList) {
-    double ema12 = 0;
-    double ema26 = 0;
+  static void _calcMACD(
+    List<KLineEntity> dataList,
+    int shortPeriod, // 12
+    int longPeriod, // 26
+    int maPeriod, // 9
+  ) {
+    double emaShort = 0;
+    double emaLong = 0;
     double dif = 0;
     double dea = 0;
     double macd = 0;
@@ -99,19 +118,21 @@ class DataUtil {
       KLineEntity entity = dataList[i];
       final closePrice = entity.close;
       if (i == 0) {
-        ema12 = closePrice;
-        ema26 = closePrice;
+        emaShort = closePrice;
+        emaLong = closePrice;
       } else {
         // EMA（12） = 前一日EMA（12） X 11/13 + 今日收盘价 X 2/13
-        ema12 = ema12 * 11 / 13 + closePrice * 2 / 13;
+        emaShort = emaShort * (shortPeriod - 1) / (shortPeriod + 1) +
+            closePrice * 2 / (shortPeriod + 1);
         // EMA（26） = 前一日EMA（26） X 25/27 + 今日收盘价 X 2/27
-        ema26 = ema26 * 25 / 27 + closePrice * 2 / 27;
+        emaLong = emaLong * (longPeriod - 1) / (longPeriod + 1) +
+            closePrice * 2 / (longPeriod + 1);
       }
       // DIF = EMA（12） - EMA（26） 。
       // 今日DEA = （前一日DEA X 8/10 + 今日DIF X 2/10）
       // 用（DIF-DEA）*2即为MACD柱状图。
-      dif = ema12 - ema26;
-      dea = dea * 8 / 10 + dif * 2 / 10;
+      dif = emaShort - emaLong;
+      dea = dea * (maPeriod - 1) / (maPeriod + 1) + dif * 2 / (maPeriod + 1);
       macd = (dif - dea) * 2;
       entity.dif = dif;
       entity.dea = dea;
@@ -149,48 +170,58 @@ class DataUtil {
     }
   }
 
-  static void _calcRSI(List<KLineEntity> dataList) {
+  static void _calcRSI(List<KLineEntity> dataList, int period) {
     double rsi;
-    double rsiABSEma = 0;
+    double rsiAbsEma = 0;
     double rsiMaxEma = 0;
     for (int i = 0; i < dataList.length; i++) {
       KLineEntity entity = dataList[i];
       final double closePrice = entity.close;
       if (i == 0) {
         rsi = 0;
-        rsiABSEma = 0;
+        rsiAbsEma = 0;
         rsiMaxEma = 0;
       } else {
-        double Rmax = max(0, closePrice - dataList[i - 1].close);
-        double RAbs = (closePrice - dataList[i - 1].close).abs();
+        double rMax = max(0, closePrice - dataList[i - 1].close);
+        double rAbs = (closePrice - dataList[i - 1].close).abs();
 
-        rsiMaxEma = (Rmax + (14 - 1) * rsiMaxEma) / 14;
-        rsiABSEma = (RAbs + (14 - 1) * rsiABSEma) / 14;
-        rsi = (rsiMaxEma / rsiABSEma) * 100;
+        rsiMaxEma = (rMax + (period - 1) * rsiMaxEma) / period;
+        rsiAbsEma = (rAbs + (period - 1) * rsiAbsEma) / period;
+        rsi = (rsiMaxEma / rsiAbsEma) * 100;
       }
-      if (i < 13) rsi = null;
-      if (rsi != null && rsi.isNaN) rsi = null;
+      if (i < (period - 1)) {
+        rsi = null;
+      }
+      if (rsi != null && rsi.isNaN) {
+        rsi = null;
+      }
       entity.rsi = rsi;
     }
   }
 
-  static void _calcKDJ(List<KLineEntity> dataList) {
+  static void _calcKDJ(
+    List<KLineEntity> dataList,
+    int calcPeriod,
+    int maPeriod1,
+    int maPeriod2,
+  ) {
     double k = 0;
     double d = 0;
     for (int i = 0; i < dataList.length; i++) {
       KLineEntity entity = dataList[i];
       final double closePrice = entity.close;
-      int startIndex = i - 13;
+      int startIndex = i - (calcPeriod - 1);
       if (startIndex < 0) {
         startIndex = 0;
       }
-      double max14 = double.minPositive;
-      double min14 = double.maxFinite;
+      double maxCalcPeriod = double.minPositive;
+      double minCalcPeriod = double.maxFinite;
       for (int index = startIndex; index <= i; index++) {
-        max14 = max(max14, dataList[index].high);
-        min14 = min(min14, dataList[index].low);
+        maxCalcPeriod = max(maxCalcPeriod, dataList[index].high);
+        minCalcPeriod = min(minCalcPeriod, dataList[index].low);
       }
-      double rsv = 100 * (closePrice - min14) / (max14 - min14);
+      double rsv =
+          100 * (closePrice - minCalcPeriod) / (maxCalcPeriod - minCalcPeriod);
       if (rsv.isNaN) {
         rsv = 0;
       }
@@ -201,11 +232,11 @@ class DataUtil {
         k = (rsv + 2 * k) / 3;
         d = (k + 2 * d) / 3;
       }
-      if (i < 13) {
+      if (i < calcPeriod - 1) {
         entity.k = null;
         entity.d = null;
         entity.j = null;
-      } else if (i == 13 || i == 14) {
+      } else if (i == calcPeriod - 1 || i == calcPeriod) {
         entity.k = k;
         entity.d = null;
         entity.j = null;
@@ -217,24 +248,24 @@ class DataUtil {
     }
   }
 
-  static void _calcWR(List<KLineEntity> dataList) {
+  static void _calcWR(List<KLineEntity> dataList, int period) {
     double r;
     for (int i = 0; i < dataList.length; i++) {
       KLineEntity entity = dataList[i];
-      int startIndex = i - 14;
+      int startIndex = i - period;
       if (startIndex < 0) {
         startIndex = 0;
       }
-      double max14 = double.minPositive;
-      double min14 = double.maxFinite;
+      double maxPeriod = double.minPositive;
+      double minPeriod = double.maxFinite;
       for (int index = startIndex; index <= i; index++) {
-        max14 = max(max14, dataList[index].high);
-        min14 = min(min14, dataList[index].low);
+        maxPeriod = max(maxPeriod, dataList[index].high);
+        minPeriod = min(minPeriod, dataList[index].low);
       }
-      if (i < 13) {
+      if (i < period - 1) {
         entity.r = -10;
       } else {
-        r = -100 * (max14 - dataList[i].close) / (max14 - min14);
+        r = -100 * (maxPeriod - dataList[i].close) / (maxPeriod - minPeriod);
         if (r.isNaN) {
           entity.r = null;
         } else {
